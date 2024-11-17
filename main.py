527710f2c74ab9ee6c8244db8c413936967a657b
Shaikh Musaib <musaibshaikh015@gmail.com>
2024-11-17 21:36:21 +0530

    port issue fix v0.1

--- main.py
+++ main.py
@@ -67,4 +67,4 @@ def delete_user(id):
         return jsonify({"message": "User not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
